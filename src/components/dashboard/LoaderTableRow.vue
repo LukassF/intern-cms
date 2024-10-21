cf9ea6d9ad75c7b37929633fe8f48e6458b822dd
LukassF <szkulkerolf@gmail.com>
2024-10-21 11:29:48 +0200

    fix

--- src/components/dashboard/LoaderTableRow.vue
+++ src/components/dashboard/LoaderTableRow.vue
@@ -8,21 +8,27 @@
       ></div>
     </td>
     <td class="flex items-center">
-      <div class="flex gap-2 items-center">
+      <div class="flex gap-1 xs:gap-2 items-center">
         <div
           :style="{ width: `${Math.round(Math.random() * 100) + 50}px` }"
-          class="h-[18px] bg-gradient-to-r from-slate-100 to-slate-200 bg-[length:400px_100px] animate-load rounded-[10px] delay-700"
+          class="max-w-[40px] xs:max-w-[50px] sm:max-w-[100%] h-[18px] bg-gradient-to-r from-slate-100 to-slate-200 bg-[length:400px_100px] animate-load rounded-[10px] delay-700"
         ></div>
         <div
           :style="{ width: `${Math.round(Math.random() * 100) + 50}px` }"
-          class="h-[18px] bg-gradient-to-r from-slate-100 to-slate-200 bg-[length:400px_100px] animate-load rounded-[10px]"
+          class="max-w-[50px] xs:max-w-[60px] sm:max-w-[100%] h-[18px] bg-gradient-to-r from-slate-100 to-slate-200 bg-[length:400px_100px] animate-load rounded-[10px]"
         ></div>
       </div>
     </td>
     <td class="flex items-center">
       <div
-        class="h-[25px] w-[50px] rounded-[10px] bg-gradient-to-r from-slate-100 to-slate-200 bg-[length:400px_100px] animate-load animation"
+        class="h-[20px] xs:h-[25px] w-[40px] xs:w-[50px] rounded-[10px] bg-gradient-to-r from-slate-100 to-slate-200 bg-[length:400px_100px] animate-load animation"
       ></div>
     </td>
   </tr>
 </template>
+
+<script setup lang="ts">
+import { useMediaQuery } from "@/utils/utils";
+
+const { matches: isXs } = useMediaQuery("(max-width: 525px)");
+</script>
